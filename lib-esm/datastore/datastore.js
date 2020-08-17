0b841646e391a6d528293d6d8767bf3de5c63c25
Yann Heeser <heeseryann@googlemail.com>
2020-08-17 13:59:23 +0200

    Fix id

--- lib-esm/datastore/datastore.js
+++ lib-esm/datastore/datastore.js
@@ -247,7 +247,7 @@ var createModelClass = function (modelDefinition) {
                 var modelInstanceMetadata = instancesMetadata.has(init)
                     ? init
                     : {};
-                var _id = modelInstanceMetadata.id, _version = modelInstanceMetadata._version, _lastChangedAt = modelInstanceMetadata._lastChangedAt, _deleted = modelInstanceMetadata._deleted;
+                var _id = init.id ? init.id : modelInstanceMetadata.id, _version = modelInstanceMetadata._version, _lastChangedAt = modelInstanceMetadata._lastChangedAt, _deleted = modelInstanceMetadata._deleted;
                 var id = 
                 // instancesIds is set by modelInstanceCreator, it is accessible only internally
                 _id !== null && _id !== undefined
